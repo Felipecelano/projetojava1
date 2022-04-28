257cdea77f6137c104ba6cec6cc088e592b75b2c
Felipe Celano <felipe.celano@hotmail.com>
2022-04-28 14:22:33 -0300

    Criação de Projeto

---
+++ projeto-java1/src/cursojava/PrimeiraClassejava.java
@@ -0,0 +1,10 @@
+package cursojava;
+
+public class PrimeiraClassejava {
+	
+	public static void main(String[] args) {		
+		System.out.println("Meu primeiro programa em java!");
+		System.out.println("Me chamo Felipe Celano!");
+	}
+
+}
