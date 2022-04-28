04b202b0dfd2246a3dffc633f4ff70b34ec09b7c
Felipe Celano <felipe.celano@hotmail.com>
2022-04-28 14:36:46 -0300

    alteração nome e comitando

--- projeto-java1/src/cursojava/PrimeiraClassejava.java
+++ projeto-java1/src/cursojava/PrimeiraClassejava.java
@@ -4,7 +4,7 @@ public class PrimeiraClassejava {
 	
 	public static void main(String[] args) {		
 		System.out.println("Meu primeiro programa em java!");
-		System.out.println("Me chamo Felipe Celano!");
+		System.out.println("Me chamo Felipe M. G. Celano!");
 	}
 
 }
